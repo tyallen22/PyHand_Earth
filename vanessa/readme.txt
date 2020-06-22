4469196d4c12d9f95b31d745dd6d7e44c46091e2
vom102 <vanessa.orantesm@gmail.com>
2020-06-22 05:31:32 -0400

    test one with google earth and pyautogui

--- vanessa/readme.txt
+++ vanessa/readme.txt
@@ -1,1 +1,3 @@
-TBD.
+1. Install pyautogui with: pip install PyAutoGUI
+2. Google earth needs to be open before running main
+
---
+++ vanessa/zoomIn.py
@@ -0,0 +1,8 @@
+import pyautogui
+import os
+import time
+
+
+#moves left
+os.system("wmctrl -a Google Earth Pro")
+pyautogui.press('=')
