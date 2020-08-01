a2038a529879431eb311fc07fc41ca5884ff594e
granthwise <grantwise@ufl.edu>
2020-07-31 20:14:42 -0400

    Changed OpenCV text

--- new_main/capture_thread.py
+++ new_main/capture_thread.py
@@ -77,55 +77,55 @@ class CaptureThread(QThread):
                 type_7_pred, type_8_pred, type_9_pred = predictions[0]
 
                 # Add text
-                type_1_text = '{}: {}%'.format(self.class_names[0], int(type_1_pred*100))
+                type_1_text = '{}: {}%'.format("Move Up", int(type_1_pred*100))
 
                 cv2.putText(frame, type_1_text, self.text_start,
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
                 # Add text
-                type_2_text = '{}: {}%'.format(self.class_names[1], int(type_2_pred*100))
+                type_2_text = '{}: {}%'.format("Move Down", int(type_2_pred*100))
 
                 cv2.putText(frame, type_2_text, (self.text_start[0], self.text_start[1] + 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
                 # Add text
-                type_3_text = '{}: {}%'.format(self.class_names[2], int(type_3_pred*100))
+                type_3_text = '{}: {}%'.format("Move Left", int(type_3_pred*100))
 
                 cv2.putText(frame, type_3_text, (self.text_start[0], self.text_start[1] + 60),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
                 # Add text
-                type_4_text = '{}: {}%'.format(self.class_names[3], int(type_4_pred*100))
+                type_4_text = '{}: {}%'.format("Move Right", int(type_4_pred*100))
 
                 cv2.putText(frame, type_4_text, (self.text_start[0], self.text_start[1] + 90),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
                 # Add text
-                type_5_text = '{}: {}%'.format(self.class_names[4], int(type_5_pred*100))
+                type_5_text = '{}: {}%'.format("Zoom In", int(type_5_pred*100))
 
                 cv2.putText(frame, type_5_text, (self.text_start[0], self.text_start[1] + 120),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
                 # Add text
-                type_6_text = '{}: {}%'.format(self.class_names[5], int(type_6_pred*100))
+                type_6_text = '{}: {}%'.format("Zoom Out", int(type_6_pred*100))
 
                 cv2.putText(frame, type_6_text, (self.text_start[0], self.text_start[1] + 150),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
                 # Add text
-                type_7_text = '{}: {}%'.format(self.class_names[6], int(type_7_pred*100))
+                type_7_text = '{}: {}%'.format("Tilt Up", int(type_7_pred*100))
 
                 cv2.putText(frame, type_7_text, (self.text_start[0], self.text_start[1] + 180),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
                 # Add text
-                type_8_text = '{}: {}%'.format(self.class_names[7], int(type_8_pred*100))
+                type_8_text = '{}: {}%'.format("Tilt Down", int(type_8_pred*100))
 
                 cv2.putText(frame, type_8_text, (self.text_start[0], self.text_start[1] + 210),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
 
                 # Add text
-                type_9_text = '{}: {}%'.format(self.class_names[8], int(type_9_pred*100))
+                type_9_text = '{}: {}%'.format("Stop Motion", int(type_9_pred*100))
 
                 cv2.putText(frame, type_9_text, (self.text_start[0], self.text_start[1] + 240),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (240, 240, 240), 2)
